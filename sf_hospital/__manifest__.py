a95d734ff507319e2a9537a2cf407a28b340b059
Peter Gichia <petergichia35@gmail.com>
2023-01-04 21:00:04 +0300

    Feature: Create the base manifest file that will register the module on odoo as an addon

---
+++ sf_hospital/__manifest__.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+# Custom module from Peter Gichia, free for use, LICENCE under MIT.
+
+
+{
+    'name': 'Hospital Management',
+    'version': '1.0.0',
+    'category': 'Hospital',
+    'summary': 'Manage hospital appointments',
+    'description': """
+This module contains all the common features of Hospital management.
+    """,
+    'depends': [],
+    'data': [],
+    'demo': [],
+    'sequence': -100,
+    'installable': True,
+    'auto_install': False,
+    'application': True,
+    'assets': {},
+    'license': 'LGPL-3',
+}
