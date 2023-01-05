5b5969a90b3df9fc914920511a9e9b7b9184ddb6
Peter Gichia <petergichia35@gmail.com>
2023-01-05 15:48:07 +0300

    Feature: Create the models folder to host all model files for the hospital module and import the patients model

---
+++ sf_hospital/models/hospital_patient.py
@@ -0,0 +1,10 @@
+from odoo import fields, models
+
+
+class HospitalPatient(models.Model):
+    _name = 'hospital.patient'
+    _description = 'Hospital Patient'
+
+    name = fields.Char(string='Patient Name', required=True)
+    age = fields.Integer(string='Age')
+    gender = fields.Selection(selection=[('male', 'Male'), ('female', 'Female')], string='Gender')
