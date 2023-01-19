d9bac98e3dd7c9c5ae58f6346f901881eca36ac8
Peter Gichia <petergichia35@gmail.com>
2023-01-19 13:20:40 +0300

    Add tracking of changes to important fileds

--- sf_hospital/models/hospital_patient.py
+++ sf_hospital/models/hospital_patient.py
@@ -6,8 +6,9 @@ class HospitalPatient(models.Model):
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Hospital Patient'
 
-    name = fields.Char(string='Patient Name', required=True)
+    name = fields.Char(string='Patient Name', required=True, tracking=True)
     ref = fields.Char(string='Reference')
-    age = fields.Integer(string='Age')
-    gender = fields.Selection(selection=[('male', 'Male'), ('female', 'Female')], string='Gender')
-    active = fields.Boolean(string='Active', default=True)
+    age = fields.Integer(string='Age', tracking=True)
+    gender = fields.Selection(
+        selection=[('male', 'Male'), ('female', 'Female')], string='Gender', tracking=True)
+    active = fields.Boolean(string='Active', default=True, tracking=True)
