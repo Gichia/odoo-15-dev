1a5fc195c8e0748050546eed202c14808c0e462f
Peter Gichia <petergichia35@gmail.com>
2023-01-19 12:53:22 +0300

    Inherit the mail and activity mixin for the chatter in UI

--- sf_hospital/models/hospital_patient.py
+++ sf_hospital/models/hospital_patient.py
@@ -3,6 +3,7 @@ from odoo import fields, models
 
 class HospitalPatient(models.Model):
     _name = 'hospital.patient'
+    _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Hospital Patient'
 
     name = fields.Char(string='Patient Name', required=True)
