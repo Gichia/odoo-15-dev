3c7b8fd0e2135f8c769a10f5cfd642b270bc6cc1
Peter Gichia <petergichia35@gmail.com>
2023-01-20 09:56:02 +0300

    Add an onchange method on the patient_id field that automatically updates the ref field

--- sf_hospital/models/hospital_appointment.py
+++ sf_hospital/models/hospital_appointment.py
@@ -1,12 +1,18 @@
-from odoo import fields, models
+from odoo import api, fields, models
 
 
 class HospitalAppointment(models.Model):
     _name = 'hospital.appointment'
     _inherit = ['mail.thread', 'mail.activity.mixin']
     _description = 'Hospital Appointments for patients'
+    _rec_name = 'patient_id'
 
     patient_id = fields.Many2one(comodel_name='hospital.patient', string='Patient', required=True)
     gender = fields.Selection(string='Gender', related="patient_id.gender")
     appointment_time = fields.Datetime(string='Appointment Time', required=True)
     booking_date = fields.Date(string='Booking Date', required=True, default=fields.Date.today)
+    ref = fields.Char(string='Reference', readonly=True)
+
+    @api.onchange('patient_id')
+    def onchange_patient_id(self):
+        self.ref = self.patient_id.ref
