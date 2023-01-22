1136ebb75a5ae73ec0a03ecacac40f130fdb1ec8
Peter Gichia <petergichia35@gmail.com>
2023-01-22 21:37:09 +0300

    Add doctor field that links to the internal users, also add priority and state fields

--- sf_hospital/models/hospital_appointment.py
+++ sf_hospital/models/hospital_appointment.py
@@ -11,9 +11,35 @@ class HospitalAppointment(models.Model):
     gender = fields.Selection(string='Gender', related="patient_id.gender")
     appointment_time = fields.Datetime(string='Appointment Time', required=True)
     booking_date = fields.Date(string='Booking Date', required=True, default=fields.Date.today)
-    ref = fields.Char(string='Reference', readonly=True)
+    ref = fields.Char(string='Reference', readonly=True, help="Reference from patient record")
     prescription = fields.Html(string='Prescription')
+    doctor_id = fields.Many2one(
+        comodel_name='res.users', string='Doctor', required=True, tracking=True)
+    priority = fields.Selection([
+        ('0', 'Normal'),
+        ('1', 'Low'),
+        ('2', 'High'),
+        ('3', 'Very High')], 
+        string="Priority", help='Indicates the priority of the appointment.')
+    state = fields.Selection([
+        ('draft', 'Draft'),
+        ('in_consultation', 'In Consultation'),
+        ('done', 'Done'),
+        ('cancel', 'Cancelled')], 
+        string="Status", default='draft', required=True, tracking=True)
 
     @api.onchange('patient_id')
     def onchange_patient_id(self):
         self.ref = self.patient_id.ref
+
+    def action_confirm(self):
+        for record in self:
+            record.state = 'in_consultation'
+
+    def action_done(self):
+        for record in self:
+            record.state = 'done'
+
+    def action_cancel(self):
+        for record in self:
+            record.state = 'cancel'
