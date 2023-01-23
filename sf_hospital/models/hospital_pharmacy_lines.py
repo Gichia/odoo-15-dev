abe5861356d3f511bc6af90270438be144b72a1e
Peter Gichia <petergichia35@gmail.com>
2023-01-23 09:16:21 +0300

    Make the price_unit field related to the product, so that the price can be picked from the product

--- sf_hospital/models/hospital_pharmacy_lines.py
+++ sf_hospital/models/hospital_pharmacy_lines.py
@@ -6,8 +6,8 @@ class HospitalPharmacyLines(models.Model):
     _description = 'Appointment pharmacy lines for patients'
 
     product_id = fields.Many2one(
-        comodel_name='product.product', string='Medicine', required=True, tracking=True)
-    price_unit = fields.Float(string='Price')
-    qty = fields.Integer(string='Quantity')
+        comodel_name='product.product', string='Medicine', required=True)
+    price_unit = fields.Float(string='Price', related='product_id.lst_price')
+    qty = fields.Integer(string='Quantity', default=1)
     appointment_id = fields.Many2one(
         comodel_name='hospital.appointment', string='Appointment')
