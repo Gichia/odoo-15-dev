fef1fafbb2d32f7b4a76c7ed439a31779c690787
Peter Gichia <petergichia35@gmail.com>
2023-01-23 07:55:54 +0300

    Add a related field for the pharmacy lines and make the ref field related to the patient_id

--- sf_hospital/models/hospital_appointment.py
+++ sf_hospital/models/hospital_appointment.py
@@ -11,7 +11,7 @@ class HospitalAppointment(models.Model):
     gender = fields.Selection(string='Gender', related="patient_id.gender")
     appointment_time = fields.Datetime(string='Appointment Time', required=True)
     booking_date = fields.Date(string='Booking Date', required=True, default=fields.Date.today)
-    ref = fields.Char(string='Reference', readonly=True, help="Reference from patient record")
+    ref = fields.Char(string='Reference', related="patient_id.ref", help="Reference from patient record")
     prescription = fields.Html(string='Prescription')
     doctor_id = fields.Many2one(
         comodel_name='res.users', string='Doctor', required=True, tracking=True)
@@ -27,6 +27,8 @@ class HospitalAppointment(models.Model):
         ('done', 'Done'),
         ('cancel', 'Cancelled')], 
         string="Status", default='draft', required=True, tracking=True)
+    pharmacy_line_ids = fields.One2many(
+        comodel_name='hospital.pharmacy.lines', inverse_name='appointment_id', string='Pharmacy Lines')
 
     @api.onchange('patient_id')
     def onchange_patient_id(self):
