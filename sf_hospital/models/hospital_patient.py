bce1ece2c17f5de0e6e73cccbfc8fccdc8a764f0
Peter Gichia <petergichia35@gmail.com>
2023-01-24 16:20:49 +0300

    Make the ref field auto generated from ir sequences, and alter the create method to update the ref field

--- sf_hospital/models/hospital_patient.py
+++ sf_hospital/models/hospital_patient.py
@@ -1,5 +1,5 @@
 from datetime import date
-from odoo import fields, models
+from odoo import _, api, fields, models
 
 
 class HospitalPatient(models.Model):
@@ -8,7 +8,8 @@ class HospitalPatient(models.Model):
     _description = 'Hospital Patient'
 
     name = fields.Char(string='Patient Name', required=True, tracking=True)
-    ref = fields.Char(string='Reference')
+    ref = fields.Char(
+        string='Order Reference', required=True, copy=False, readonly=True, default=lambda self: _('New'))
     dob = fields.Date(string='Date Of Birth', required=True, tracking=True)
     age = fields.Integer(string='Age', compute='_compute_age', tracking=True)
     gender = fields.Selection(
@@ -16,6 +17,17 @@ class HospitalPatient(models.Model):
     active = fields.Boolean(string='Active', default=True, tracking=True)
     image = fields.Image(string='Image')
     tag_ids = fields.Many2many(comodel_name='hospital.patient.tag', string='Tags')
+
+    @api.model
+    def create(self, vals):
+        #Add your custom code here
+        vals['ref'] = self.env['ir.sequence'].next_by_code('hospital.patient') or _('New')
+        return super(HospitalPatient, self).create(vals)
+
+    def write(self, vals):
+        #Add your custom code here
+        # print('Writing::::::::::::')
+        return super(HospitalPatient, self).write(vals)
 
     def _compute_age(self):
         today = date.today()
